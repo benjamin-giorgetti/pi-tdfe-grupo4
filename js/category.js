5fe3d13123c57f71b3b6b4c5c48a303b131ab3eb
benjamin-giorgetti <gbenjamin@udesa.edu.ar>
2026-06-13 20:31:18 -0300

    productos por cateogira dinamicos

---
+++ js/category.js
@@ -0,0 +1,44 @@
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let categoria = queryStringObj.get("category");
+
+let categoriaTitulo = document.querySelector(".categoriaTitulo");
+let productosCategoria = document.querySelector(".productosCategoria");
+
+if (categoria == null) {
+    categoriaTitulo.innerHTML = "Categoría no seleccionada";
+    productosCategoria.innerHTML = "<p>No hay productos para mostrar.</p>";
+} else {
+    categoriaTitulo.innerHTML = categoria;
+
+    fetch("https://dummyjson.com/products?limit=0")
+        .then(function(respuesta) {
+            return respuesta.json();
+        })
+        .then(function(data) {
+            let productos = data.products;
+            let cantidadProductos = 0;
+
+            for (let i = 0; i < productos.length; i++) {
+                let producto = productos[i];
+
+                if (producto.category == categoria) {
+                    productosCategoria.innerHTML += `
+                        <article class="producto">
+                            <img src="${producto.thumbnail}" alt="${producto.title}">
+                            <h3>${producto.title}</h3>
+                            <p class="precio">$${producto.price}</p>
+                            <p>${producto.description}</p>
+                            <div class="btnVerMas"><a href="./product.html?id=${producto.id}">Ver detalle</a></div>
+                        </article>
+                    `;
+
+                    cantidadProductos++;
+                }
+            }
+
+            if (cantidadProductos == 0) {
+                productosCategoria.innerHTML = "<p>No hay productos para la categoría seleccionada.</p>";
+            }
+        });
+}
